36a493950ae9dfc032107d698bdfcc8e0a553004
gnanesh2310 <kgnanesh98@gmail.com>
2025-05-29 16:23:32 +0530

    Add files via upload

---
+++ src/learnpage/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js  
+import React from 'react';  
+import LearnSection  from './LearnSection.jsx'; 
+import './App.css';
+
+
+const App = () => {  
+  return (  
+    <div className="App">  
+      <LearnSection />  
+    </div>  
+  );  
+};  
+
+export default App;
