05192213afc93d62da62d7746c88d954ee4c2bde
Rugved Tipare <rugved.tipare25@gmail.com>
2025-08-15 23:27:55 +0530

    Fix Routing

--- src/component/tesla-learn-page/src/App.jsx
+++ src/component/tesla-learn-page/src/App.jsx
@@ -1,15 +1,14 @@
-// src/App.js  
-import React from 'react';  
-import LearnSection  from './LearnSection.jsx'; 
-import './App.css';
+// src/App.js
+import React from "react";
+import LearnSection from "./LearnSection.jsx";
+import "./App.css";
 
-
-const App = () => {  
-  return (  
-    <div className="App">  
-      <LearnSection />  
-    </div>  
-  );  
-};  
+const App = () => {
+  return (
+    <div className="App">
+      <LearnSection />
+    </div>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Hero from './component/Hero'
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Achieve from "./component/Achieve";
+import Council from "./component/Council";
+import Projects from "./component/projests-page/src/App.jsx";
+import Gallery from "./component/Gallery";
+import Learn from "./component/tesla-learn-page/src/App.jsx";
+import Nav from "./component/Nav.jsx";
+import Footer from "./component/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <>
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <App />
+    <Nav />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="achieve" element={<Achieve />} />
+      <Route path="council" element={<Council />} />
+      <Route path="gallery" element={<Gallery />} />
+      <Route path="learn" element={<Learn />} />
+    </Routes>
+    <Footer />
   </BrowserRouter>
-  
-
-  </>
-  
-    
-  
-)
+);
